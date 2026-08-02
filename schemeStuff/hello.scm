c1e0e25282dcead27c27774cfd8596a4d969fbff
elem <elementarycoder1@gmail.com>
2026-08-02 16:01:37 +0530

    study progress on the Little lisper

--- schemeStuff/hello.scm
+++ schemeStuff/hello.scm
@@ -263,9 +263,38 @@
       (else (add1 (// (minus n m)
                       m))))))
 
+(define length_
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
 
+(define x '(1 2 3 4 5))
+(length x)
+(length_ x)
 
+(define pick
+  (lambda (n lat)
+    (cond
+      ((eq n 1)
+       (car lat))
+      (else (pick (sub1 n)
+                  (cdr lat))))))
 
+(pick 5 x)
+(define y '(lasagna spaghetti ravioli macaroni meatball))
+(pick 4 y)
+(pick 0 y)
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons (car lat)
+                  (rempick (sub1 n)
+                           (cdr lat)))))))
+(rempick 1 y)
+(rempick 3 y)
 
 
 
